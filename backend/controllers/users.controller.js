605c68277432d074ac49219901fbebb3230f845f
Julian Pinzon <julianchos1998@gmail.com>
2021-09-05 20:34:42 -0500

    deleteUserPockets

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -137,9 +137,23 @@ const UsersController = {}
                 //delete (listar usuarios)
                 UsersController.deleteUser=(req,res)=>{
 
-                  Users.deleteOne({_id:req.body.id}).then((result)=>{
-                    res.status(200).json({message:'User eliminado'})
-                  })
+                    Pockets.find({duenio: req.body.id}).then((pocketsUser)=>{
+
+                      listPockets = pocketsUser;
+                      for(let i = 0; i < listPockets.length; i++){
+
+                        let pocket1 = listPockets[i];
+
+                        Pockets.deleteOne({_id: pocket1._id}).then((rae)=>{
+                          console.log('eliminado')
+                        })
+                      }
+
+                    })
+
+                    Users.deleteOne({_id:req.body.id}).then((result)=>{
+                      res.status(200).json({message:'User eliminado'})
+                    })
 
                 };
 
