ce6a3893927e502b0def9fd6b192e0b8f1a85cbd
Julian Pinzon <julianchos1998@gmail.com>
2021-09-05 19:38:55 -0500

    correccionBackendPago

--- backend/controllers/pockets.controller.js
+++ backend/controllers/pockets.controller.js
@@ -129,60 +129,57 @@ const PocketsController = {}
 
         PocketsController.pagoUsers= (req,res)=>{
 
-          Users.find({rol:req.body.rol}).then((result)=>{
-            if(result){
-              let listUsers= result;
-              let totalAPagar = listUsers.length*2000000;
+          Users.find({rol: req.body.rol}).then((userResults)=>{
 
-              Pockets.find({duenio:req.userData.userId}).then((pocketsResult)=>{
-                              if(pocketsResult){
+            let listUsers = userResults;
+            let totalAPagar = listUsers.length * 2000000
 
-                                for(let i =0; i< pocketsResult.length; i++){
-                                  if(pocketsResult[i].name=='NÓMINA'){
+            Pockets.find({duenio: req.userData.userId}).then((pocketResults)=>{
 
-                                    if(pocketsResult[i].saldo>=totalAPagar){
+              if(pocketResults){
 
-                                      let pocket1 = pocketsResult[i];
-                                      pocket1.saldo = Number(pocket1.saldo) - totalAPagar;
-                                      Pockets.updateOne({_id:pocket1._id},pocket1).then((rea)=>{
+                for(let i =0; i<pocketResults.length;i++){
+                  if(pocketResults[i].name=='NÓMINA'){
+                    if(pocketResults[i].saldo >= totalAPagar){
 
+                      let pocket1 = pocketResults[i];
+                      pocket1.saldo = Number(pocket1.saldo) - totalAPagar;
 
+                      Pockets.updateOne({_id: pocket1._id},pocket1).then((rea)=>{
 
-                                        for(let i = 0; i< listUsers.length;i++){
+                        for(let j = 0;j < listUsers.length; j++){
 
-                                          Pockets.find({duenio:listUsers[i]._id}).then((pocketsResult)=>{
-                                            if(pocketsResult){
-                                              for(let j =0; j< pocketsResult.length; j++){
-                                                if(pocketsResult[j].name=='DISPONIBLE'){
-                                                  let pocket1 = pocketsResult[j];
-                                                  pocket1.saldo = Number(pocket1.saldo) + 2000000;
-                                                  Pockets.updateOne({_id:pocket1._id},pocket1).then((rea)=>{
-                                                    console.log('disponible++')
+                          let user1 = listUsers[j];
 
+                          Pockets.find({duenio: user1._id}).then((pocketsResult)=>{
+                            if(pocketsResult){
+                              for( let h = 0; h < pocketsResult.length; h++){
+                                if(pocketsResult[h].name == 'DISPONIBLE'){
 
-                                                  })
-                                                }
-                                              }
+                                  let pocket2 = pocketsResult[h];
+                                  pocket2.saldo = Number(pocket2.saldo) + 2000000;
 
-                                            }
-                                          })
-
-                                        }
-                                          })
-
-                                      }else{
-                                        res.status(409).json({message:'el valor a pagar es superior a los fondos en nomina.'})
-                                      }
-
-                                  }
+                                  Pockets.updateOne({_id: pocket2._id},pocket2).then((rae)=>{
+                                    console.log('disponible++');
+                                  })
                                 }
                               }
-
-                            })
+                            }
+                          })
+                        }
+                        res.status(200).json({message:'pago efectuado.'})
+                      })
+                    }else{
+                      res.status(409).json({message:'el valor a pagar es superior a los fondos en nomina.'});
+                    }
+                  }
+                }
               }
-            }).then(()=>{
-              res.status(200).json({message:'pago efectuado.'})
+
             })
+
+          })
+
 
 
         }
